b03ae35c7a40f65573870935204b2f8d49dd1034
Rifat Azad <rifat.44.azad.rifs@gmail.com>
2025-08-11 17:20:34 +0600

    peridot: update product model and adjust TWRP alignment settings

--- twrp_peridot.mk
+++ twrp_peridot.mk
@@ -16,13 +16,14 @@ PRODUCT_RELEASE_NAME := peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_NAME := twrp_peridot
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := RedmiTurbo3
+PRODUCT_MODEL := PocoF6
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := peridot
 
-# Theme
+# TWRP - Punch hole alignments
 TW_STATUS_ICONS_ALIGN := center
-TW_Y_OFFSET := 99
-TW_H_OFFSET := -99
+TW_CUSTOM_CPU_POS := "50"
+TW_CUSTOM_CLOCK_POS := "340" 
+TW_CUSTOM_BATTERY_POS := "800"
